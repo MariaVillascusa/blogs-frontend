1165243a444555056701994a760fa873d969080b
Maria Villascusa <meri.villa14@gmail.com>
2021-09-10 11:57:31 +0200

    5.9 bloglist frontend step9

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ const App = () => {
                 <BlogForm addBlog={addBlog} />
               </Togglable>
               <div className="bloglist">
-                {blogs.map(blog =>
+                {(blogs.sort((a, b) => b.likes - a.likes)).map(blog =>
                   <Blog key={blog.id} blog={blog} />
                 )}
               </div>
