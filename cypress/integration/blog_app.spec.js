d7fe67cdabe2dde8db7e1fd150d0946acb466bf5
Maria Villascusa <meri.villa14@gmail.com>
2021-10-05 00:24:37 +0200

    5.18 bloglist e2e step2

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'user',
+      username: 'user',
+      password: 'password'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,4 +15,23 @@ describe('Blog app', function () {
     cy.contains('Log in to application')
     cy.contains('Login')
   })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('[name="Username"]').type('user')
+      cy.get('[name="Password"]').type('password')
+      cy.get('.login-btn').click()
+      cy.contains('Log out')
+      cy.contains('New blog')
+    })
+    it('fails with wrong credentials', function() {
+      cy.get('[name="Username"]').type('user')
+      cy.get('[name="Password"]').type('wrongpassword')
+      cy.get('.login-btn').click()
+      cy.get('.error')
+        .should('contain', 'Wrong username o password')
+        .and('have.css', 'background-color', 'rgb(221, 80, 68)')
+    })
+  })
 })
+
